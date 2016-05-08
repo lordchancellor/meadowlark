a1f313743741aaa86bb28755093e590b9e7a2061
Christopher Ford <christopher@ChristohersMini.home>
2016-05-08 20:02:43 +0100

    Extracted the fortune cookie code into its own module

--- meadowlark.js
+++ meadowlark.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fortune = require('./lib/fortune.js');
 
 var app = express();
 
@@ -16,8 +17,7 @@ app.get('/', function(req, res) {
 });
 
 app.get('/about', function(req, res) {
-    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', { fortune: randomFortune });
+    res.render('about', { fortune: fortune.getFortune() });
 });
 
 //Custom 404 Page
@@ -34,13 +34,4 @@ app.use(function(req, res) {
 
 app.listen(app.get('port'), function() {
     console.log(' Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate');
-})
-
-//Fortune cookie array
-var fortunes = [
-    "Conquer your fears or they will conquer you.",
-    "Rivers need springs.",
-    "Do not fear what you do not know.",
-    "You will have a pleasent surprise.",
-    "Whenever possible, keep it simple"  
-];
+});
