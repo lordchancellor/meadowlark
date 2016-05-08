c8e38e178921f4364121d944c07952e55fd27422
Christopher Ford <christopher@ChristohersMini.home>
2016-05-08 16:36:35 +0100

    Handlebars added as view engine and basic view templates established

--- meadowlark.js
+++ meadowlark.js
@@ -2,30 +2,33 @@ var express = require('express');
 
 var app = express();
 
+//Setup Handlebars view engine
+var handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
 app.set('port', process.env.PORT || 3000);
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res) {
-    res.type('text/plain');
-    res.send('Meadowlark Travel');
+    res.render('home');
 });
 
 app.get('/about', function(req, res) {
-    res.type('text/plain');
-    res.send('About Meadowlark Travel');
-})
+    res.render('about');
+});
 
 //Custom 404 Page
 app.use(function(req, res) {
-    res.type('text/plain');
     res.status(404);
-    res.send('404 - Not Found');
+    res.render('404');
 });
 
 //Custom 500 Page
 app.use(function(req, res) {
-    res.type('text/plain');
     res.status(500);
-    res.send('500 - Server Error');
+    res.render('500');
 });
 
 app.listen(app.get('port'), function() {
