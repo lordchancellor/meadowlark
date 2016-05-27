e7b2041656c9bb917e6d0853208b1109b8e91c59
lordchancellor <c.d.j.ford@gmail.com>
2016-05-27 21:54:13 +0100

    Implemented the section helper, which has allowed the Request Group Rate page to start working

--- meadowlark.js
+++ meadowlark.js
@@ -4,7 +4,19 @@ var fortune = require('./lib/fortune.js');
 var app = express();
 
 //Setup Handlebars view engine
-var handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
+var handlebars = require('express-handlebars').create({ 
+    defaultLayout: 'main',
+    helpers: {
+        section: function(name, options) {
+            if (!this._sections) {
+                this._sections = {};
+            }
+            this._sections[name] = options.fn(this);
+            return null;
+        }
+    }
+});
+
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
